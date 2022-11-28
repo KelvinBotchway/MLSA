d01e7590f068c898249e958ee6bd759a318684fc
Kelvin <kelvinjay1000@gmail.com>
2022-11-28 21:06:52 +0000

    Hello

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counterd from './components/conterd';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter value = {5}/>
+      <Counter value = {15}/>
+      <Counter value = {30}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import "./counter.css"
+
+const Counter = (props) => {
+
+  const [count, setCount] = useState(0)
+
+
+    console.log(props)
+
+  const increase = () => {
+    setCount(count + props.value)
+    console.log(count)
+  }
+  const decrease = () => {
+    setCount(count - props.value)
+    console.log(count)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase} className="button"> Increase + {props.value}</button>
+      <button onClick={decrease} className="button">Decrease - {props.value}</button>
+    </div>
+    
+
+  )
+}
+
+export default Counter
